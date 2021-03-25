bb2e1f69be8e176d34368dc65bd62b8d03de63b8
Mariojj <mariojj@live.com>
2021-03-25 02:51:17 +0100

    implementando más la interfaz de usuario

--- proyectoPrueba/Equipos/src/menu/interfaz.java
+++ proyectoPrueba/Equipos/src/menu/interfaz.java
@@ -1,6 +1,8 @@
 package menu;
 
 import java.util.Scanner;
+import modelo.*;
+import xml.*;
 
 public class interfaz {
 	
@@ -8,8 +10,8 @@ public class interfaz {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("1.- Exportar toda la lista de equipos");
-		System.out.println("2.- Importar toda la lista de equipos");
+		System.out.println("1.- Exportar todos los equipos de futbol");
+		System.out.println("2.- Importar todos los equipos de futbol");
 		System.out.println("3.- Elegir un equipo y exportarlo");
 		System.out.println("4.- Importar un equipo");
 		System.out.println("5.- Validar un archivo XML de un equipo");
@@ -22,10 +24,42 @@ public class interfaz {
 	    System.out.println("12.- Listar equipos");
 	    System.out.println("");
 	    System.out.println("Elige una opcion:");
-	    Scanner in = new Scanner(System.in);
-	    
-	    int opcion = in.nextInt();
-	    System.out.println("Elegida opción "+opcion);
+	    try (Scanner in = new Scanner(System.in)) {
+			int opcion = in.nextInt();
+			System.out.println("Elegida opción "+opcion);
+			
+			switch(opcion) {
+				case 1:System.out.println("Exportando archivo");
+				
+				break;
+				case 2:System.out.println("Introduzca el nombre del archivo que desea importar:");
+				       Scanner entradaArchivo = new Scanner(System.in);
+			           String nombreArchivo = entradaArchivo.nextLine();
+			           System.out.println("Importando "+nombreArchivo);
+				break;
+				case 3:System.out.println("Introduzca el nombre del equipo que desea exportar:");
+				break;
+				case 4:;
+				break;
+				case 5:;
+				break;
+				case 6:;
+				break;
+				case 7:;
+				break;
+				case 8:;
+				break;
+				case 9:;
+				break;
+				case 10:;
+				break;
+				case 11:;
+				break;
+				case 12:;
+				break;
+				default:
+			}
+		}
 	}
 
 }
